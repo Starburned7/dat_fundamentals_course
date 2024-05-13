d82dc273430825fef968d2180bbeea032ed559a1
Zarema Sivkova <starburned777@gmail.com>
2024-05-13 19:33:03 +0000

    test commit

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -14,3 +14,6 @@ select
     status
 
 from {{ source('jaffle_shop', 'orders') }}
+
+
+{{ limit_data_in_dev('order_date', 3000) }}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,17 +1,17 @@
-select * 
+-- select * 
 
-from {{ source('stripe', 'payment') }}
+-- from {{ source('stripe', 'payment') }}
 
 
 
--- select
---     id as payment_id,
---     orderid as order_id,
---     paymentmethod as payment_method,
---     status,
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
---     -- amount is stored in cents, convert it to dollars
---     amount / 100 as amount,
---     created as created_at
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount') }} as amount,
+    created as created_at
 
--- from raw.stripe.payment 
+from raw.stripe.payment 
